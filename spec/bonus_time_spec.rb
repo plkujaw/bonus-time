43fe93071b56a4d88051ebd96408c8c8f62efd86
plkujaw <jawor.jakub@gmail.com>
2020-05-13 12:29:14 +0000

    added one more test for false

--- spec/bonus_time_spec.rb
+++ spec/bonus_time_spec.rb
@@ -5,5 +5,6 @@ describe 'bonus_time' do
     expect(bonus_time(69, true)).to eq('$690')
     expect(bonus_time(100, false)).to eq('$100')
     expect(bonus_time(2, true)).to eq('$20')
+    expect(bonus_time(5000, false)).to eq('$5000')
   end
 end
